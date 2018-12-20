5b691333835190053baa2f511c426ce259dc6ed9
biplavt <biplavt@hawsco.com>
2018-12-20 15:45:10 -0800

    Added correct SF configuation

--- Salesforce/configSF.js
+++ Salesforce/configSF.js
@@ -2,8 +2,8 @@ module.exports={
 	oauth: {
       // you can change loginUrl to connect to sandbox or prerelease env.
       loginUrl: 'https://test.salesforce.com/',
-      clientId: '3MVG9MHOv_bskkhQKsOfEdjGIWrWajLB2pf_QqbXZQZhEXGBNlP2NxNzBStxHH4augZU7vuG.FchltxVAIVrd',
-      clientSecret: '2261574018905486495',
+      clientId: '3MVG9MHOv_bskkhQKsOfEdjGIWhKix3dkqo5Ok_XYiLwqcvtAVUt0KT7RgoGrmkFNd..7EOb80LpWXA._bHFs',
+      clientSecret: '8844925097654278054',
       redirectUri: 'http://localhost:3000/'
       
     },
