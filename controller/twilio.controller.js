b2c6926cc18722d9b3054388a597e601a3be1f05
biplavt <biplavt@hawsco.com>
2019-01-23 10:08:18 -0800

    minor update twilio

---
+++ controller/twilio.controller.js
@@ -0,0 +1,79 @@
+var AX2009Model = require('./../model/AX2009.model');
+var configTwilio = require('./../configuration/twilio.config');
+
+
+//require the Twilio module 
+var twilio = require('twilio');
+
+var accountSid = configTwilio.accountSid;
+var authToken = configTwilio.authToken;
+var client = new twilio(accountSid, authToken);
+
+var reminder = function(req,res) {
+
+	var name;
+	var phone;
+	var email;
+	// var bod;
+	console.log('salesid:',req.params.salesId);
+
+	AX2009Model.getCustomerDetailsFromSalesId(req,res).then(function(result){
+
+		if(result.recordsets[0].length !== 0){
+			
+			name = JSON.stringify(result.recordsets[0][0].Customer),
+			phone = JSON.stringify(result.recordsets[0][0].PHONE),
+			email = JSON.stringify(result.recordsets[0][0].EMAIL)
+			console.log('name:',name, 'phone',phone,'email',email);
+			// bod=name + phone + email;	
+		}
+		
+		const bod = name + phone + email;	
+		console.log('body',bod);
+		
+		client.messages.create({ 
+
+		    to: "7756007864", 
+		    from: "+18504800786",
+		    body: `New order from customer. Customer Details: ${bod}`, 
+		  
+		  }, function(err, message) { 
+		  
+		    console.log(err); 
+		  
+		  });
+		
+		res.send('message sent');
+	
+	},function(error){
+	
+		console.log(error);
+	
+	})
+
+};
+
+
+var call = function(req,res){
+	
+	const client = require('twilio')(accountSid, authToken);
+
+	client.calls.create(
+	  {
+	    url: 'http://demo.twilio.com/docs/voice.xml',
+	    to: '7754209818',
+	    from: '+18504800786',
+	  },
+
+	  (err, call) => {
+	    process.stdout.write(call.sid);
+	  }
+
+	);
+
+}
+
+module.exports = {
+	reminder,
+	call
+}
